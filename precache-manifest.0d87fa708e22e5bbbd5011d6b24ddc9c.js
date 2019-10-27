3d42bb64da7f50c98788647a893d88b841e7375e
agylling <agylling@kth.se>
2019-10-27 15:19:43 +0100

    Updates

---
+++ precache-manifest.0d87fa708e22e5bbbd5011d6b24ddc9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98ec38b50ee4cc4d80e437ed2d8eef0",
+    "url": "/TradeSummaryApp/index.html"
+  },
+  {
+    "revision": "2f6cd41a6c5c217b3e5b",
+    "url": "/TradeSummaryApp/static/css/main.de384b6c.chunk.css"
+  },
+  {
+    "revision": "aae83bb3e0904b5e1770",
+    "url": "/TradeSummaryApp/static/js/2.22309701.chunk.js"
+  },
+  {
+    "revision": "2f6cd41a6c5c217b3e5b",
+    "url": "/TradeSummaryApp/static/js/main.f17aba38.chunk.js"
+  },
+  {
+    "revision": "4d674de18e7583bb7d66",
+    "url": "/TradeSummaryApp/static/js/runtime-main.0211e248.js"
+  }
+]);
