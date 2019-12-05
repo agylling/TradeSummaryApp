8e4b7c14363043dda13fa181ad8e904c4d9f3244
agylling <andreasgylling69@gmail.com>
2019-12-05 20:22:29 +0100

    Updates

---
+++ precache-manifest.5aa3780d5cef37cb58f69932a919ded2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1592782273f5169c28cb49a792dc1e26",
+    "url": "/TradeSummaryApp/index.html"
+  },
+  {
+    "revision": "63c365f3e2e1cbe3c8d3",
+    "url": "/TradeSummaryApp/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ff613169370db07e3df1",
+    "url": "/TradeSummaryApp/static/css/main.9b31ee2c.chunk.css"
+  },
+  {
+    "revision": "63c365f3e2e1cbe3c8d3",
+    "url": "/TradeSummaryApp/static/js/2.179ddbdd.chunk.js"
+  },
+  {
+    "revision": "ff613169370db07e3df1",
+    "url": "/TradeSummaryApp/static/js/main.80811133.chunk.js"
+  },
+  {
+    "revision": "4d674de18e7583bb7d66",
+    "url": "/TradeSummaryApp/static/js/runtime-main.0211e248.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TradeSummaryApp/precache-manifest.2a739f63d4dd73b3c3157315621d40c0.js"
+  "/TradeSummaryApp/precache-manifest.5aa3780d5cef37cb58f69932a919ded2.js"
 );
 
 self.addEventListener('message', (event) => {
